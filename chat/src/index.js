e8b93d875a6f40de000c658fe8c10cb3fa187c2f
Man-Green-Tea <iamnambavan@gmail.com>
2022-02-09 20:35:53 +0300

    add redux

--- chat/src/index.js
+++ chat/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { Header } from "./components";
+import { store } from "./store";
 import { ChatPage, ProfilePage } from "./pages";
 import "./global.css";
 
@@ -16,18 +18,19 @@ const theme = createTheme({
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header />
-
-        <Routes>
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/chat/*" element={<ChatPage />} />
-          <Route path="/" element={<h1>Home page</h1>} />
-          <Route path="/*" element={<h1>404</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/chat/*" element={<ChatPage />} />
+            <Route path="/" element={<h1>Home page</h1>} />
+            <Route path="/*" element={<h1>404</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ chat/src/store/profile/reducer.js
@@ -0,0 +1,16 @@
+import { TOGGLE_VISIBLE_PROFILE } from "./types";
+
+const initialState = {
+  firstName: "firstName",
+  lastName: "lastName",
+  isVisibleProfile: true,
+};
+
+export const profileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_VISIBLE_PROFILE:
+      return { ...state, isVisibleProfile: !state.isVisibleProfile };
+    default:
+      return state;
+  }
+};
